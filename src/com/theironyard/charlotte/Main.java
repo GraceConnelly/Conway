da71aa8e9b827ea0ee42ee918287f6a057232554
GraceConnelly <grace.connelly.a@gmail.com>
2017-01-02 11:52:45 -0500

    iterates through copy array, determines if cells are alive or dead and stores the status in a working array

--- src/com/theironyard/charlotte/Main.java
+++ src/com/theironyard/charlotte/Main.java
@@ -3,8 +3,10 @@ package com.theironyard.charlotte;
 import java.io.*;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.StringJoiner;
 
 public class Main {
+
 
     private static String[][] getFileContents(String fileName) {
         File f = new File(fileName);
@@ -37,13 +39,72 @@ public class Main {
     }
 
     private static int getAliveNeighbors(String[][] array, int i, int j) {
-        // TODO: implement this
-        return 42;
+        // method determines Moore's neighborhood of alive neighbors
+        int aliveCnt = 0;
+
+            for (int k = i-1; k <= i+1 ; k++) {
+                for (int l = j-1; l <= j+1; l++) {
+                    if(k >= 0 && k < array.length && l >= 0 && l < array[k].length) {  //takes care of array boundaries
+                        if (array[k][l].equals("1")) {
+                            aliveCnt++;
+                        }
+                    }
+                }
+            }
+            if(array[i][j].equals("1")){
+                aliveCnt--;
+            }
+        return aliveCnt;
+    }
+    private static boolean livesOrDies(int aliveCnt, boolean isAlive){
+//        Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
+//        Any live cell with two or three live neighbours lives on to the next generation.
+//        Any live cell with more than three live neighbours dies, as if by overpopulation.
+//        Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+        switch (aliveCnt){
+            case 0:
+            case 1:
+                if(isAlive == true) {
+                    isAlive = false;
+                }
+                break;
+            case 2:
+            case 3:
+                if(isAlive == false && aliveCnt == 3){
+                    isAlive = true;
+                }
+                break;
+            default:
+                isAlive = false;
+        }
+        return isAlive;
     }
 
-    public static void main(String[] args) throws IOException {
-        // TODO: Implement this.
 
+    public static void main(String[] args) throws IOException {
+        // create a copy of the input.txt array to work with
+        String[][] copy = deepArrayCopy(getFileContents("input.txt"));
+        //TODO: Build while loop to go through specified number of times
+
+
+        String[][] workingCopy = copy;
+        //Iterate through the entire copy array. Determine if the cell live or dies.Store the status in the workingCopy array
+        for (int i = 0; i < copy.length; i++) {
+            for (int j = 0; j < copy[i].length; j++) {
+                int aliveCnt = getAliveNeighbors(copy,i,j);
+                boolean isAlive = (copy[i][j].equals("1"));
+                isAlive = livesOrDies(aliveCnt, isAlive);
+                //store in working Copy
+                if (isAlive) {
+                    workingCopy[i][j] = "1";
+                }
+                else workingCopy[i][j]= "0";
+            }
+
+        }
+        copy = workingCopy;
+
+        //TODO: compare copy to output
         // note: you should be able to use Arrays.deepEquals to
         // compare these arrays
     }
